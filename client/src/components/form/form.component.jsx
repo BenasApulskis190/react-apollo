5003824e35cf178de0d6c047c5705dd6587e912c
Benas Apulskis <benas@ito.lt>
2020-08-13 02:24:24 +0300

    improving input component

--- client/src/components/form/form.component.jsx
+++ client/src/components/form/form.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Input from '../input/input.component';
 import Button from '../button/button.component';
 
@@ -6,19 +6,18 @@ import './form.style.scss'
 
 const Form = () => {
 
+  const [credentials, setCredentials] = useState({
+    name: '',
+    lastName: '',
+    message: '',
+  });
+
   return (
     <div className="Form">
       <form>
-        <Input label="First name" id="name" name="name" placeholder="First name" type="text"/>
-        <Input label="Last name" id="lastName" name="lastName" placeholder="Last name" type="text"/>
-
-        <div className="form-group">
-          <label htmlFor="message">Message</label>
-          <textarea name="message" id="message" cols="30" rows="7" placeholder="Your message here">
-
-          </textarea>
-        </div>
-
+        <Input inputType="input" label="First name" id="name" name="name" placeholder="First name" type="text"/>
+        <Input inputType="input" label="Last name" id="lastName" name="lastName" placeholder="Last name" type="text"/>
+        <Input inputType="textarea" label="message" name="message" id="message" cols="30" rows="7" placeholder="Your message here"/>
         <Button text="Submit" type="button"/>
       </form>
     </div>
--- client/src/components/input/input.component.jsx
+++ client/src/components/input/input.component.jsx
@@ -2,11 +2,28 @@ import React from 'react';
 
 import './input.style.scss';
 
-const Input = ({label, id, ...props}) => (
-  <div className="form-group">
-    {label && (<label htmlFor={id}>{label}</label>)}
-    <input {...props}/>
-  </div>
-);
+const Input = ({label, id, inputType, ...props}) => {
+  let inputElement;
+
+  switch (inputType) {
+    case ('input') :
+      inputElement = <input {...props}/>
+      break;
+
+    case ('textarea') :
+      inputElement = <textarea {...props} />
+      break;
+
+    default :
+      inputElement = <input {...props}/>
+  }
+
+  return (
+    <div className="form-group">
+      {label && (<label htmlFor={id}>{label}</label>)}
+      {inputElement}
+    </div>
+  )
+};
 
 export default Input;
